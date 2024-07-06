e4d3628767b0842dc86591a429c7091f774bd56a
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-07-06 12:45:30 +0100

    deplo2

--- config/database.js
+++ config/database.js
@@ -1,21 +1,25 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config(); // Load environment variables from .env file
 
-require("dotenv").config(); // Load environment variables from .env fileconst 
- //let sequelize;
+let sequelize;
 
 // For production deployment on Render.com or similar
 
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Adjust based on your database configuration
+      },
     },
-  },
-});
-
+  }
+);
 
 // // For local development
 // sequelize = new Sequelize(
