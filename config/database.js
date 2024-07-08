1a49fa146679d9a7dd6d856db5ffa969f400c6d0
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-07-08 12:23:56 +0100

    added connection pool

--- config/database.js
+++ config/database.js
@@ -15,6 +15,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
   logging: false,
+  pool: {
+    max: 10, // Maximum number of connections in the pool
+    min: 0, // Minimum number of connections in the pool
+    acquire: 30000, // Maximum time (in ms) that pool will try to get connection before throwing error
+    idle: 10000, // Maximum time (in ms) that a connection can be idle before being released
+  },
 });
 // // For local development
 // sequelize = new Sequelize(
