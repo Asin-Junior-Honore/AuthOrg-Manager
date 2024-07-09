d96df5c7b9e41af5cb58b68ad7dc67eec23e6898
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-07-10 00:37:36 +0100

    added a root route

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { sequelize } = require("./config/database");
-const routes = require("./routes");
 const dotenv = require("dotenv");
+const routes = require("./routes");
 
 dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.send("👋 Hello yea go on and explore the app thanks ✌️");
+});
+
 app.use("/", routes);
 
 sequelize.sync().then(() => {
